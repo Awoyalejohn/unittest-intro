d8d9d1ea1673f2dc25800a70fd228540782dee12
John Awoyale <awoyalejohn@gmail.com>
2022-05-12 00:11:52 +0100

    tests

---
+++ test_evens.py
@@ -0,0 +1,9 @@
+import unittest
+from evens import even_number_of_evens
+
+class TestEvens(unittest.TestCase):
+    def test_throws_error_if_value_passed_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
+    
+if __name__ == "__main__":
+    unittest.main()
